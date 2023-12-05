691e5f1fdc23e012cdd911988f578b3335c1280a
flyck <felix.brilej@googlemail.com>
2023-12-05 20:18:42 +0100

    feat(day-04): add part two of the day one challenge

--- 04/main.test.ts
+++ 04/main.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "bun:test";
-import { scratchcardPoints } from "./main";
+import { scratchcardPoints, scratchCardCopyPoints } from "./main";
 
 describe("Part 1 Testsuite", () => {
   test("Example Input", () => {
@@ -18,4 +18,22 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`
     expect(scratchcardPoints(input)).toBe(23235);
   });
 
+})
+
+describe("Part 2 Testsuite", () => {
+  test("Example Input", () => {
+    const input = `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`
+
+    expect(scratchCardCopyPoints(input)).toBe(13);
+  });
+
+  test("Input text", async () => {
+    const input = await Bun.file("input.txt").text()
+    expect(scratchCardCopyPoints(input)).toBe(5920640);
+  });
 })
--- 04/main.ts
+++ 04/main.ts
@@ -36,3 +36,49 @@ function parseNumbers(input: string): number[] {
     .map((elem) => parseInt(elem))
     .filter((elem) => !isNaN(elem))
 }
+
+
+export function scratchCardCopyPoints(input: string) {
+  let sum = 0
+
+  const scratchcards = input.split("\n")
+  let reservedCopy: { [key: number]: number } = {}
+  scratchcards.forEach((scratchcard, index) => {
+    let points = 0
+
+    if (!scratchcard) return
+
+    const [title, numbers] = scratchcard.split(":")
+    const [winners, drawn] = numbers.split("|")
+
+    const winnerNumbers = parseNumbers(winners)
+    const drawnNumbers = parseNumbers(drawn)
+
+    drawnNumbers.forEach((number) => {
+      if (winnerNumbers.includes(number)) {
+        points++
+      }
+    })
+
+    // get if we have extra copies
+    let extraCopies = reservedCopy[index] ?? 0
+
+    // for each match we have, add one copy to that number of following cards, multiplied with the
+    // extra copies we have of our current card
+    for (let walkingIndex = 1; points > 0; points--) {
+      console.debug(`Adding ${1 + extraCopies} copies of the card ${index + walkingIndex + 1}`)
+      if (!reservedCopy[index + walkingIndex]) {
+        reservedCopy[index + walkingIndex] = 1 + extraCopies
+      } else {
+        reservedCopy[index + walkingIndex] += 1 + extraCopies
+      }
+
+      walkingIndex++
+    }
+
+    console.debug(`Had a multiplier of ${extraCopies}, adding that many points to the sum`)
+    sum += 1 + extraCopies
+  })
+
+  return sum
+}
