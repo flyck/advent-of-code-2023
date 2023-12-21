85fc5449ca46979e626aa475a2c2069dc32c5a77
flyck <felix.brilej@googlemail.com>
2023-12-21 19:34:42 +0100

    chore(day-07): work towards completion of day-07

---
+++ 07/index.test.ts
@@ -0,0 +1,31 @@
+import { describe, test, expect } from "bun:test"
+import { Rank, determineRank, sortCards, totalScore } from "./index"
+
+
+describe("Testsuite part 2", () => {
+  test("Example Input", () => {
+    const input = `32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483`
+    const it = totalScore(input)
+    expect(it).toEqual(6440)
+  })
+
+  test("Ranking", () => {
+    expect(determineRank("T55J5")).toEqual({ rank: Rank.THREE_OF_A_KIND, cards: ["5"] })
+    expect(determineRank("26543")).toEqual({ rank: Rank.HIGH_CARD, cards: ["6"] })
+  })
+
+  test("Sorting", () => {
+    expect(sortCards("25436".split(""))).toEqual("65432".split(""))
+    expect(sortCards("52243".split(""))).toEqual("54322".split(""))
+  })
+
+  test("Input file", async () => {
+    const input = await Bun.file("input.txt").text()
+    const it = totalScore(input)
+    expect(it).toBe(253728865)
+  })
+})
---
+++ 07/index.ts
@@ -0,0 +1,150 @@
+import { FILE } from "dns"
+
+const ORDER = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
+
+export function totalScore(input: string): number {
+  let score = 0
+
+  const placements = input.split("\n").filter((line) => line != "")
+
+  let ranks: RankAndCardAndBid[] = []
+
+  // get all ranks and their related card
+  placements.forEach((placement) => {
+    const [hand, bid] = placement.split(" ")
+    const rank = determineRank(hand)
+    ranks.push({ ...rank, bid: parseInt(bid) })
+  })
+
+  console.log(placements.length)
+  //console.log("RANKS: " + JSON.stringify(ranks))
+
+  let multiplier = placements.length
+  Object.values(Rank).forEach((rank, asd) => {
+    // console.log("Rank: " + asd)
+    const myRankResults = ranks.filter((rankAndCard) => rankAndCard.rank == rank)
+    // console.log(myRankResults)
+
+    if (myRankResults.length == 1) {
+      score += multiplier * myRankResults[0].bid
+    }
+
+    if (myRankResults.length > 1) {
+      // BUGBUG change sorting logic to simply compare cards by index
+      const sorted = sortResults(myRankResults)
+
+      console.log("Sorted result: " + JSON.stringify(sorted))
+
+      sorted.forEach((entry) => {
+        // console.log(`Adding number: ${multiplier} * ${entry.bid}`)
+        score += multiplier * entry.bid
+        multiplier--
+      })
+    }
+  })
+
+  console.log(multiplier)
+
+  return score
+}
+
+// all ranks from highest to lowest
+export enum Rank {
+  FIVE_OF_A_KIND = "FIVE_OF_A_KIND",
+  FOUR_OF_A_KIND = "FOUR_OF_A_KIND",
+  FULL_HOUSE = "FULL_HOUSE",
+  THREE_OF_A_KIND = "THREE_OF_A_KIND",
+  TWO_PAIR = "TWO_PAIR",
+  ONE_PAIR = "ONE_PAIR",
+  HIGH_CARD = "HIGH_CARD",
+}
+
+interface RankAndCardAndBid { rank: Rank, cards: string[], bid: number }
+
+export function sortResults(input: RankAndCardAndBid[]): RankAndCardAndBid[] {
+  return input.sort(compareCards)
+}
+
+function compareCards(a: RankAndCardAndBid, b: RankAndCardAndBid): number {
+  const cardsA = a.cards.map((card) => ORDER.indexOf(card));
+  const cardsB = b.cards.map((card) => ORDER.indexOf(card));;
+
+  // Compare each card number
+  for (let i = 0; i < cardsA.length; i++) {
+    if (cardsA[i] !== cardsB[i]) {
+      return cardsA[i] - cardsB[i]; // Higher number wins
+    }
+  }
+
+  // If all card numbers are the same, compare the next property (if any)
+  // For example, if you have another property called "name", you can add:
+  // return a.name.localeCompare(b.name);
+
+  return 0; // Objects are considered equal
+}
+
+export function determineRank(hand: string): { rank: Rank, cards: string[] } {
+  const cards = hand.split("")
+
+  const occurrences = getOccurrences(hand)
+
+  let foundPairs: string[] = []
+  let foundThree = ""
+  Object.entries(occurrences).forEach(([card, occurrence]) => {
+    if (occurrence == 5) {
+      return { rank: Rank.FIVE_OF_A_KIND, cards: [card] }
+    }
+    if (occurrence == 4) {
+      return { rank: Rank.FOUR_OF_A_KIND, cards: [card] }
+    }
+    if (occurrence == 3) {
+      foundThree = card
+    }
+    if (occurrence == 2) {
+      foundPairs.push(card)
+    }
+  })
+
+  // full house check
+  if (foundThree != "" && foundPairs.length == 1) {
+    return { rank: Rank.FULL_HOUSE, cards: [foundThree, foundPairs[0]] }
+  }
+
+  // three of a kind check
+  if (foundThree != "" && foundPairs.length == 0) {
+    return { rank: Rank.THREE_OF_A_KIND, cards: [foundThree] }
+  }
+
+  // two pairs check
+  if (foundPairs.length == 2) {
+    return { rank: Rank.TWO_PAIR, cards: sortCards(foundPairs) }
+  }
+
+  // one pair check
+  if (foundPairs.length == 1) {
+    return { rank: Rank.ONE_PAIR, cards: foundPairs }
+  }
+
+  // high card is the default
+  console.log(sortCards(cards))
+  return { rank: Rank.HIGH_CARD, cards: sortCards(cards).slice(0, 1) }
+}
+
+// returns the cards sorted by ascii code in descending order
+export function sortCards(cards: string[]): string[] {
+  return cards.sort((a: string, b: string) => ORDER.indexOf(a) - ORDER.indexOf(b))
+}
+
+function getOccurrences(hand: string): { [key: string]: number } {
+  let occurrences: { [key: string]: number } = {}
+
+  hand.split("").forEach((card) => {
+    if (occurrences[card]) {
+      occurrences[card] += 1
+    } else {
+      occurrences[card] = 1
+    }
+  })
+
+  return occurrences
+}
